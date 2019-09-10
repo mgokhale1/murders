60f3ba6f1eb100685ce5ee41fead034ad716bf56
Milind Gokhale <milindgokhale@admins-MacBook-Pro.local>
2019-09-10 18:04:25 -0500

    committing changes

---
+++ download-data.R
@@ -0,0 +1,11 @@
+#url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+#dest_file <- "data/murders.csv"
+#download.file(url,destfile = dest_file)
+
+
+url <- "https://raw.githubusercontent.com/rairizarry/murders/master/report.Rmd"
+dest_file <- "reports/report.rmd"
+download.file(url,destfile = dest_file)
+
+
+
